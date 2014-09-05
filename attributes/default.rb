d291f7560bf75f8b813ea7c1811ffd021e9585b2
Jason Barnett <jason.w.barnett@gmail.com>
2014-09-04 17:45:20 -0700

    package name that contains the ca-certificates is different between RHEL 5.x and 6.x

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,16 @@
 case node['platform_family']
-when 'rhel'
-  default['ca-certificates']['package'] = 'ca-certificates'
-  default['ca-certificates']['ca-bundle_path'] = '/etc/pki/tls/certs/ca-bundle.crt'
-else
-  default['ca-certificates']['package'] = 'ca-certificates'
-  default['ca-certificates']['ca-bundle_path'] = '/etc/pki/tls/certs/ca-bundle.crt'
+  when 'rhel'
+    default['ca-certificates']['ca-bundle_path'] = '/etc/pki/tls/certs/ca-bundle.crt'
+    case node['platform_version']
+      when /^5\./
+        default['ca-certificates']['package'] = 'openssl'
+      when /^6\./
+        default['ca-certificates']['package'] = 'ca-certificates'
+      else
+        default['ca-certificates']['package'] = 'ca-certificates'
+      end
+  else
+    default['ca-certificates']['package'] = 'ca-certificates'
+    default['ca-certificates']['ca-bundle_path'] = '/etc/pki/tls/certs/ca-bundle.crt'
 end
+
