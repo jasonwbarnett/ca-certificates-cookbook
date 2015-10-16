c64f0cc540b28dc5c4bc67960a8d620c451cb7cd
Jason Barnett <jason.w.barnett@gmail.com>
2015-10-16 09:24:47 -0700

    bumped to 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'J@sonBarnett.com'
 license          'MIT'
 description      'Installs/Configures ca-certificates'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 %w( redhat centos amazon scientific oracle debian ).each do |os|
   supports os
